4d3fa624a9ef03925d6bcf0c6fba949c39c7b1fe
Kei Tashima <tassi.yuzukko@gmail.com>
2017-05-29 05:20:04 +0900

    LINQ to SQLの自動SQL文作成について、どんな文が作られているかをログ出力してみた

--- DbTest/LinqToSqlTest.cs
+++ DbTest/LinqToSqlTest.cs
@@ -12,9 +12,12 @@ namespace DbTest
 		{
 			var db = new LinqToSql.LinqToSqlTestDataContext();
 
-			var query = db.members.Where(m => m.name == "hoge").Select(n => n);
+			var query = db.members.Where(m => m.name == "hoge");
 
-			foreach(var m in query )
+			// DEBUG: 内部でどういうSQL文が作られているかを、log出力
+			db.Log = Console.Out;
+
+			foreach (var m in query )
 			{
 				Console.WriteLine("{0} {1} {2}", m.id, m.birthday, m.name);
 			}
